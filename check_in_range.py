131fc0dea2a8ec2a7b4aa9c9636c7f3f1795d956
cyril <cyril.auberger@gmail.com>
2024-03-12 15:35:19 +0100

    Refactored & cleaned codebase

--- check_in_range.py
+++ check_in_range.py
@@ -1,41 +1,38 @@
+import csv
+import os
 import requests
+
 from mav import *
 
-# Your webhook URL
-webhook_url = 'https://discord.com/api/webhooks/1215240065572282399/scjLgu4683hhSvDFsPSP_yUU21QvleuJ5JRXOY2FHrZ7SMCEKXQ1Uymzm2nlFfmnzMT-'
+with open('owners_networks_pools.csv', 'r') as read_obj: 
+    # 'owners_networks_pools.csv' is a csv with 3 columns: 
+    # 1. owner (address of the LP user), 
+    # 2. network (at the moment "mainnet" and "zksync" are supported)
+    # 3. pool (0x address of the pool)
+    csv_reader = csv.reader(read_obj) 
+    owners_networks_pools = list(csv_reader) 
 
-# Random owner of a position on Maverick Mainnet
-owner = HexBytes(0x37Ff9172beCAf3B78284822eD55a03FAB0Bc9E27)
-pool = HexBytes(0xd50c68c7fbaee4f469e04cebdcfbf1113b4cdadf)
+for line in owners_networks_pools[1:]: # Remove the title line
+    owner = HexBytes(line[0])
+    network = line[1]
+    pool = HexBytes(line[2])
+    position_inspector_contract = PositionInspectorContract(network)
+    out_of_range = position_inspector_contract.is_out_of_range(owner, pool)
+    pool_information_contract = PoolInformationContract(network)
+    pool_information_contract.get_price(pool, network)
+    pool_information_contract.get_active_tick_start_and_end_price(pool, network)
 
-is_out_of_range = is_out_of_range(owner, pool)
+    if out_of_range:
+        data = {
+            "content": f"Owner {owner.hex()} is out of range on pool {pool.hex()}",
+            "username": "Custom Notification Bot"
+        }
 
-if is_out_of_range:
-    data = {
-        "content": f"Owner {owner.hex()} is out of range on pool {pool.hex()}",
-        "username": "Custom Notification Bot"
-    }
+        # POST request to the Discord webhook
+        response = requests.post(webhook_url, json=data)
 
-    # POST request to the Discord webhook
-    response = requests.post(webhook_url, json=data)
-
-    # Check the response
-    if response.status_code == 204:
-        print("Notification sent successfully!")
-    else:
-        print("Failed to send notification.")
-else:
-    # TO REMOVE
-    data = {
-        "content": f"Owner {owner.hex()} is in of range on pool {pool.hex()}",
-        "username": "Custom Notification Bot"
-    }
-
-    # POST request to the Discord webhook
-    response = requests.post(webhook_url, json=data)
-
-    # Check the response
-    if response.status_code == 204:
-        print("Notification sent successfully!")
-    else:
-        print("Failed to send notification.")
+        # Check the response
+        if response.status_code == 204:
+            logging.info("Notification sent successfully! Owner %s is out of range on pool %s", owner.hex(), pool.hex())
+        else:
+            logging.error("Failed to send notification for owner %s on pool %s. Status code: %d", owner.hex(), pool.hex(), response.status_code)
--- mav.py
+++ mav.py
@@ -1,94 +1,97 @@
-import datetime
-import os
-import sys
-from decimal import Decimal
-
-from eth_account import Account
-from eth_account.signers.local import LocalAccount
-from web3 import HTTPProvider, Web3
 from hexbytes import HexBytes
+import logging
+from web3 import HTTPProvider, Web3
 
-from mav_contracts import *
+from config import *
 
-infura_key = "a4be4c2bdcd3460a96e969fc9376db9b"
 
-# account: LocalAccount = Account.from_key(private_key)
-# my_address = account.address
-
-network = 'mainnet'
-
-# Connect to JSON-RPC node
-json_rpc_url = f'https://{network}.infura.io/v3/{infura_key}'
-
-web3 = Web3(HTTPProvider(json_rpc_url))
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 class MavContract(object):
   def __init__(self, network, name):
-    self.abi = abis[name]
-    self.address = addresses[network][name]
-    self.contract = web3.eth.contract(address=self.address, abi=self.abi)
+    self.network = network
+    self.name = name
+    self.web3 = Web3(HTTPProvider(json_rpc_urls[self.network]))
+    self.abi = abis[self.name]
+    self.address = addresses[self.network][self.name]
+    self.contract = self.web3.eth.contract(address=self.address, abi=self.abi)
+    
+  def from_wei(self, balance):
+    """Converts balance from gwei to ether."""
+    return self.web3.from_wei(balance, 'ether')
 
 
-PositionInspector = MavContract(network, "PositionInspector")
-PoolInformation = MavContract(network, "PoolInformation")
-
-def get_balance_from_gwei(balance):
-  # Divide by 10**18
-  return web3.from_wei(balance, 'ether')
-
-def get_balances(owner, pool):
-  # NB : Might be a problem if an owner has more than one position on a single pool
-  balances = PositionInspector.contract.functions.addressBinReservesAllKindsAllTokenIds(owner=owner, pool=pool).call()
-  balance_a = get_balance_from_gwei(balances[0])
-  balance_b = get_balance_from_gwei(balances[1])
-  print(f"Balance of token A: {balance_a}")
-  print(f"Balance of token B: {balance_b}")
-  return balance_a, balance_b
-
-def get_price(pool):
-  sqrt_price = PoolInformation.contract.functions.getSqrtPrice(pool=pool).call()
-  price = get_balance_from_gwei(sqrt_price) ** 2
-  print(f"Price of asset B in asset A: {price}")
-  return price
+class PositionInspectorContract(MavContract):
+    """Subclass of MavContract specialized for the PositionInspector contract."""
+    def __init__(self, network, name='PositionInspector'):
+        super().__init__(network, 'PositionInspector')
 
 
-def get_active_tick_start_and_end_price(pool):
-# Take all active bins, and loop through them, and find the bin with both A and B tokens > this is bin of the current price
-# Then take bin ID - 1, and + 1 to get the min and max of the range of the active bin
-  all_active_bins = PoolInformation.contract.functions.getActiveBins(pool=pool, startBinIndex=0, endBinIndex=0).call()
+    def get_balances(self, owner, pool):
+        """Fetches balances for a given owner and pool."""
+        logging.info(f"Fetching token balances for owner {owner.hex()} on pool {pool.hex()}")
+        balances = self.contract.functions.addressBinReservesAllKindsAllTokenIds(owner=owner, pool=pool).call()
+        # NB : Might be a problem if an owner has more than one position in a single pool
 
-  for active_bin in all_active_bins:
-    bin_id = active_bin[0]
-    bin_kind = active_bin[1]
-    bin_lower_tick = active_bin[2]
-    bin_reserve_a = active_bin[3]
-    bin_reserve_b = active_bin[4]
-    #mergeId ? cf https://etherscan.io/address/0x0087D11551437c3964Dddf0F4FA58836c5C5d949#code
-    if bin_reserve_a != 0 and bin_reserve_b != 0:
-      print(f"Lower tick of active bin is {bin_lower_tick}")
-      break
+        balance_a = self.from_wei(balances[0])
+        balance_b = self.from_wei(balances[1])
 
-  tickLiquidityBefore = PoolInformation.contract.functions.tickLiquidity(pool=pool, tick=bin_lower_tick-1).call()
-  active_tick_start_price = get_balance_from_gwei(tickLiquidityBefore[0]) ** 2
-  tickLiquidityAfter = PoolInformation.contract.functions.tickLiquidity(pool=pool, tick=bin_lower_tick+1).call()
-  active_tick_end_price = get_balance_from_gwei(tickLiquidityAfter[0]) ** 2
-  print(f"Pool active tick Start Price is {active_tick_start_price}")
-  print(f"Pool active tick End Price is {active_tick_end_price}")
-  return active_tick_start_price, active_tick_end_price
+        logging.info(f"Balance of token A: {balance_a}")
+        logging.info(f"Balance of token B: {balance_b}")
 
+        return balance_a, balance_b
 
-def is_out_of_range(owner, pool):
-  balance_a, balance_b = get_balances(owner, pool)
-  if balance_a == 0 or balance_b == 0:
-    print(f"Owner {owner.hex()} is out of range on pool {pool.hex()}")
-    return True
-  print(f"Owner {owner.hex()} is in range on pool {pool.hex()}")
-  return False
+    def is_out_of_range(self, owner, pool):
+        """Determines if the owner is out of range for the specified pool."""
+        balance_a, balance_b = self.get_balances(owner, pool)
+
+        in_range_msg = f"Owner {owner.hex()} is in range on pool {pool.hex()}"
+        out_of_range_msg = f"Owner {owner.hex()} is out of range on pool {pool.hex()}"
+
+        if balance_a == 0 or balance_b == 0:
+            logging.info(out_of_range_msg)
+            return True
+
+        logging.info(in_range_msg)
+        return False
 
 
-# get_balances(owner, pool)
-# get_price(pool)
-# get_active_tick_start_and_end_price(pool)
-# is_out_of_range(owner, pool)
+class PoolInformationContract(MavContract):
+    """Subclass of MavContract specialized for the PositionInspector contract."""
+    def __init__(self, network, name='PoolInformation'):
+        super().__init__(network, 'PoolInformation')
 
+
+    def get_price(self, pool, network):
+      """Gets the current price of the pair"""
+      sqrt_price = self.contract.functions.getSqrtPrice(pool=pool).call()
+      price = self.from_wei(sqrt_price) ** 2
+      logging.info(f"Price of asset B in asset A: {price}")
+      return price
+
+
+    def get_active_tick_start_and_end_price(self, pool, network):
+      # Take all active bins, and loop through them, and find the bin with both A and B tokens > this is bin of the current price
+      # Then take bin ID - 1, and + 1 to get the min and max of the range of the active bin
+      all_active_bins = self.contract.functions.getActiveBins(pool=pool, startBinIndex=0, endBinIndex=0).call()
+
+      for active_bin in all_active_bins:
+        bin_id = active_bin[0]
+        bin_kind = active_bin[1]
+        bin_lower_tick = active_bin[2]
+        bin_reserve_a = active_bin[3]
+        bin_reserve_b = active_bin[4]
+        #mergeId ? cf https://etherscan.io/address/0x0087D11551437c3964Dddf0F4FA58836c5C5d949#code
+        if bin_reserve_a != 0 and bin_reserve_b != 0:
+          logging.info(f"Lower tick of active bin is {bin_lower_tick}")
+          break
+
+      tickLiquidityBefore = self.contract.functions.tickLiquidity(pool=pool, tick=bin_lower_tick-1).call()
+      active_tick_start_price = self.from_wei(tickLiquidityBefore[0]) ** 2
+      tickLiquidityAfter = self.contract.functions.tickLiquidity(pool=pool, tick=bin_lower_tick+1).call()
+      active_tick_end_price = self.from_wei(tickLiquidityAfter[0]) ** 2
+      logging.info(f"Pool active tick Start Price is {active_tick_start_price}")
+      logging.info(f"Pool active tick End Price is {active_tick_end_price}")
+      return active_tick_start_price, active_tick_end_price
